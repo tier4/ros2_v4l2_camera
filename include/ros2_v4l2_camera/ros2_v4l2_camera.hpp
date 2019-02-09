5fbaa469407808f3a12f2887c11f5195dea69a69
Sander van Dijk <sgvandijk@gmail.com>
2019-02-09 16:49:14 +0000

    Runnable node

--- include/ros2_v4l2_camera/ros2_v4l2_camera.hpp
+++ include/ros2_v4l2_camera/ros2_v4l2_camera.hpp
@@ -1,12 +1,27 @@
+// Copyright 2019 Bold Hearts
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef ROS2_V4L2_CAMERA__ROS2_V4L2_CAMERA_HPP_
 #define ROS2_V4L2_CAMERA__ROS2_V4L2_CAMERA_HPP_
 
+#include <rclcpp/rclcpp.hpp>
 #include "ros2_v4l2_camera/visibility_control.h"
 
 namespace ros2_v4l2_camera
 {
 
-class Ros2V4L2Camera
+class Ros2V4L2Camera : public rclcpp::Node
 {
 public:
   Ros2V4L2Camera();
