e548b4a336c005da38229d2a592d99bcdc34dfca
Sander G. van Dijk <sgvandijk@gmail.com>
2019-07-05 14:49:36 +1000

    Uncrustify

--- src/v4l2_camera.cpp
+++ src/v4l2_camera.cpp
@@ -142,12 +142,12 @@ void V4L2Camera::createParameters()
           break;
         }
       case ControlType::MENU:
-      {
-        // TODO: treating as integer parameter, implement full menu functionality
-        auto value = declare_parameter<int64_t>(name, camera_->getControlValue(c.id));
-        camera_->setControlValue(c.id, value);
-        break;
-      }
+        {
+          // TODO: treating as integer parameter, implement full menu functionality
+          auto value = declare_parameter<int64_t>(name, camera_->getControlValue(c.id));
+          camera_->setControlValue(c.id, value);
+          break;
+        }
       default:
         RCLCPP_WARN(get_logger(),
           std::string{"Control type not currently supported: "} + std::to_string(unsigned(c.type)) +
